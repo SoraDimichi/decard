11fa66159ab72823815edcb4f686a9aaa6bf2ee2
Dmitry Sorokin <soradimichi@gmail.com>
2025-08-21 22:20:55 +0600

    disconected user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env-validate';
-import { PrismaService } from './prisma.service';
 import { PaparaModule } from './papara/papara.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ validate, isGlobal: true }), PaparaModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
